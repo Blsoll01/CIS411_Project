9e0afee155e99456c67ce4caae90ccb9c860b156
blsoll01 <katanaweapon@yahoo.com>
2013-04-23 09:47:43 -0400

    Latest changes to the program

---
+++ PROJECT/StudyCards/StudyCards/Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace StudyCards.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string EventTitle { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventDesc { get; set; }
+        public string EventLocation { get; set; }
+        public int CourseID { get; set; }
+    }
+
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        //public DbSet<Event_Registry> Event_Registry { get; set; }
+
+        //public DbSet<Registry> Registries { get; set; }
+    }
+}
